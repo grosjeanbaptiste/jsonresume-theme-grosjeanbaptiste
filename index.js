b3455fbcc4f7ccbe10a91bf744336a062896c82f
Baptiste Grosjean <grosjeanbaptiste@outlook.com>
2024-10-14 08:06:41 +0200

    tree set

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const fs = require('fs');
 const Handlebars = require('handlebars');
-Handlebars.registerHelper('eq', function(a, b) {
-  return a === b;
-});
 const path = require('path');
+const fetch = require('node-fetch'); 
+
+const gistUrl = 'https://gist.githubusercontent.com/grosjeanbaptiste/06b06d74ae3ae6d02dc1d6b018e54a75/raw/resume.json';
 
 module.exports = {
-  render: function(resume) {
-    const template = fs.readFileSync(path.join(__dirname, 'resume.hbs'), 'utf-8');
-    const tpl = Handlebars.compile(template);
-
-    const css = fs.readFileSync(path.join(__dirname, 'style.css'), 'utf-8');
-
-    return tpl({
-      resume: resume,
-      css: css
-    });
+  render: async function() {
+    try {
+      const response = await fetch(gistUrl);
+      const resume = await response.json();
+  
+      const template = fs.readFileSync(path.join(__dirname, 'resume.hbs'), 'utf-8');
+      const tpl = Handlebars.compile(template);
+  
+      const css = fs.readFileSync(path.join(__dirname, 'style.css'), 'utf-8');
+  
+      return tpl({
+        resume: resume,
+        css: css
+      });
+    } catch (error) {
+      console.error('Error fetching resume.json from Gist:', error);
+      throw error;
+    }
   }
 };
